f3484245351d5d77e227cb8172da70048f515d1a
aljan russmor <ajrussmor1@gmail.com>
2023-01-19 11:55:21 -0500

    modified label2

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Doolie, you have no todo items yet! Add one above!</p>
+                <p className="text-center">Doolie7, you have no todo items yet! Add one above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
